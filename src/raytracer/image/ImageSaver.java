e1b4b9bd95275ad7181e3477460ebffe74d1c485
Simon Lischka <simon@a-studios.org>
2013-11-07 00:20:32 +0100

    Implement quick and dirty line drawing

--- src/raytracer/image/ImageSaver.java
+++ src/raytracer/image/ImageSaver.java
@@ -1,23 +1,25 @@
 package raytracer.image;
 
-import java.awt.Dimension;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.image.BufferedImage;
 
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
 public class ImageSaver {
 	public static void main(String[] args) {
-		JFileChooser chooser = new JFileChooser();
-//		if (chooser.showOpenDialog(null) != JFileChooser.APPROVE_OPTION) {
-//			System.exit(0);
-//		}
-		//BufferedImage image = ImageIO.read(chooser.getSelectedFile());
 		BufferedImage image = new BufferedImage(100, 50,
                 BufferedImage.TYPE_INT_ARGB);
-        JFrame frame = new JFrame();
-        frame.setSize(800, 600);
-        frame.getContentPane().add(new ImageCanvas(image));
+        final JFrame frame = new JFrame();
+        frame.setSize(640, 480);
+        final ImageCanvas drawing = new ImageCanvas(image);
+        frame.getContentPane().add(drawing);
+        frame.addComponentListener(new ComponentAdapter() {
+        	public void componentResized(ComponentEvent e) {
+        		drawing.setSize(frame.getSize());
+        		frame.repaint();
+        	}
+        });
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);	
 	}
