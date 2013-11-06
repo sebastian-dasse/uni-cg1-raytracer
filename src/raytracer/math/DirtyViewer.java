c0191fd0f4d0a99aae26b42027880c168a5f0769
Max Novichkov <s500@beuth-hochschule.de>
2013-11-06 11:56:47 +0100

    Small changes in class DirtyViewer

--- src/raytracer/math/DirtyViewer.java
+++ src/raytracer/math/DirtyViewer.java
@@ -1,10 +1,8 @@
 package raytracer.math;
 
-import java.awt.Desktop;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
