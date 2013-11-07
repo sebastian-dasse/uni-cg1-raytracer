ca02e8634baeb97c92156c2d98068e267c0be995
Simon Lischka <simon@a-studios.org>
2013-11-07 01:05:27 +0100

    Quick and dirty image saver

--- src/raytracer/image/ImageSaver.java
+++ src/raytracer/image/ImageSaver.java
@@ -1,19 +1,57 @@
 package raytracer.image;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import raytracer.ui.FileDialog;
+
 
 public class ImageSaver {
-	public static void main(String[] args) {
-		BufferedImage image = new BufferedImage(100, 50,
+	public static void main(String[] args) throws IOException {
+		final BufferedImage image = new BufferedImage(100, 50,
                 BufferedImage.TYPE_INT_ARGB);
         final JFrame frame = new JFrame();
+        
+        JMenuBar menubar = new JMenuBar();
+        JMenu menu = new JMenu("File");
+        JMenuItem saveItem = new JMenuItem("Save");
+        menu.add(saveItem);
+        menubar.add(menu);
+        frame.setJMenuBar(menubar);
+        
+        /* 
+         * Doesn't work jet - saves red mini image. Mega Quick and Dirty,
+         * has to be refactored. 
+         */
+        saveItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					ImageIO.write(image, "jpg", FileDialog.save());
+				}
+				catch (IOException ie) {
+					
+				}
+			}
+        });
+        
         frame.setSize(640, 480);
         final ImageCanvas drawing = new ImageCanvas(image);
         frame.getContentPane().add(drawing);
+        
+        /*
+         * Listener could be refactored too - look for intelligent solution 
+         * for ListenerClasses.
+         */
         frame.addComponentListener(new ComponentAdapter() {
         	public void componentResized(ComponentEvent e) {
         		drawing.setSize(frame.getSize());
@@ -22,6 +60,9 @@ public class ImageSaver {
         });
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);	
+        
 	}
+
+
 
 }
