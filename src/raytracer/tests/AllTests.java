e8e02b22677b20310c6d67876b96262eade738d4
Simon Lischka <simon@a-studios.org>
2013-11-09 23:35:33 +0100

    Add tests + too puristic way of math test

---
+++ src/raytracer/tests/AllTests.java
@@ -0,0 +1,17 @@
+package raytracer.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+
+public class AllTests {
+	public static Test suite() {
+		TestSuite suite = new TestSuite();
+		suite.addTestSuite(MathTests.class);
+		return suite;
+	}
+	
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+}
