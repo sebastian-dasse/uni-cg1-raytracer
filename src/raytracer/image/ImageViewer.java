5fe4b6c3339fb51343f39c30478e5f4c0810a37c
sebastian-dasse <s50602@beuth-hochschule.de>
2013-11-11 20:07:22 +0100

    Fix ImageViewer to automatically fit frame to image.

--- src/raytracer/image/ImageViewer.java
+++ src/raytracer/image/ImageViewer.java
@@ -1,12 +1,12 @@
 package raytracer.image;
 
-import java.awt.Canvas;
-import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import raytracer.ui.FileDialog;
 
@@ -14,17 +14,9 @@ public class ImageViewer {
 	public static void main(String[] args) throws IOException {
 		final BufferedImage image = ImageIO.read(FileDialog.open());
 		final JFrame frame = new JFrame();
-		final Canvas canvas = new Canvas() {
-			private static final long serialVersionUID = 1L;
-
-			public void paint(final Graphics g) {
-				super.paint(g);
-				g.drawImage(image, 0, 0, null);
-			}
-		};
-		frame.getContentPane().add(canvas);
+		frame.getContentPane().add(new JLabel(new ImageIcon(image)));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(image.getHeight(), image.getWidth());
+		frame.pack();
 		frame.setVisible(true);
 	}
 }
