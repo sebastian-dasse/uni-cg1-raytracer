3d8b6ad0a73bff3d9a211014006084dc7eb055a8
Simon Lischka <simon@a-studios.org>
2013-11-11 21:40:22 +0100

    Start commenting on image canvas

--- src/raytracer/image/ImageCanvas.java
+++ src/raytracer/image/ImageCanvas.java
@@ -1,5 +1,14 @@
 package raytracer.image;
-
+/**
+ * This immutable class represents the canvas for a diagonal line on black background.
+ * When initalized, it generates an BufferedImage with the dimension defined in its constructor. 
+ * 
+ * The class includes a paint method which is called to display the generated image.
+ * With the getter and setter methods the BufferedImage can be retrieved or
+ * modified in its size.
+ * 
+ * @author Simon Lischka
+ */
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -11,13 +20,25 @@ import java.awt.image.WritableRaster;
 public final class ImageCanvas extends Canvas {
 	private Dimension size;
 	private BufferedImage image;
-
+/**
+ * Constructor, generates internal ImageBuffer with passed over size parameters, 
+ * sets ColorModel to TYPE_INT_RGB.
+ * 
+ * @param size
+ */
 	public ImageCanvas(Dimension size) {
 		this.size = size;
 		image = new BufferedImage((int)size.getWidth(), (int)size.getHeight(),
 				BufferedImage.TYPE_INT_RGB);
 	}
-	
+
+/**
+ * Constructor, generates internal ImageBuffer with passed over size parameters, 
+ * sets ColorModel to TYPE_INT_RGB.
+ * 
+ * @param width
+ * @param height
+ */
 	public ImageCanvas(int width, int height) {
 		this(new Dimension(width, height));
 	}
@@ -25,13 +46,25 @@ public final class ImageCanvas extends Canvas {
 	public BufferedImage getImage() {
 		return image;
 	}
-	
+
+/**
+ * Reinitializes the BufferedImage with new size parameters.
+ * 
+ * @param size
+ */
 	public void setSize(Dimension size) {
 		this.size = size;
 		image = new BufferedImage((int)size.getWidth(), (int)size.getHeight(),
 				BufferedImage.TYPE_INT_RGB);
 	}
 
+/**
+ * Generates a black background and red diagonal line. The data is 
+ * written into the raster of the classes BufferedImage and then
+ * painted.
+ * 
+ * @param g
+ */
 	public void paint(final Graphics g) {
 		super.paint(g);
 		final WritableRaster raster = image.getRaster();
