939d67459d38be342308a13e1ac0325088e2c523
sebastian-dasse <s50602@beuth-hochschule.de>
2013-11-12 11:25:46 +0100

    Prepare some geometry classes.

--- src/raytracer/geometry/Geometry.java
+++ src/raytracer/geometry/Geometry.java
@@ -1,5 +1,28 @@
 package raytracer.geometry;
 
-public class Geometry {
+import raytracer.Color;
+import raytracer.Ray;
 
+/**
+ * @author 
+ *
+ */
+public abstract class Geometry {
+	/**
+	 * 
+	 */
+	public final Color color;
+	
+	/**
+	 * @param color
+	 */
+	public Geometry(final Color color) {
+		this.color = color;
+	}
+	
+	/**
+	 * @param ray
+	 * @return
+	 */
+	public abstract Hit hit(final Ray ray);
 }
