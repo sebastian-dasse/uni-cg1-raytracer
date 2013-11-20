a954188e1d2f1316dc6bfe5b4e5948dd12a0953f
sebastian-dasse <s50602@beuth-hochschule.de>
2013-11-20 16:23:46 +0100

    Color is done.

--- src/raytracer/Color.java
+++ src/raytracer/Color.java
@@ -1,5 +1,7 @@
 package raytracer;
 
+import static java.lang.Math.min;
+import static java.lang.Math.max;
 import static raytracer.math.MathUtil.inRange0To1;
 
 /**
@@ -39,7 +41,9 @@ public class Color {
 	 * @return
 	 */
 	public Color add(final Color c) {
-		return null;
+		return new Color(min(1, r + c.r), 
+						 min(1, g + c.g), 
+						 min(1, b + c.b));
 	}
 	
 	/**
@@ -47,7 +51,9 @@ public class Color {
 	 * @return
 	 */
 	public Color sub(final Color c) {
-		return null;
+		return new Color(max(0, r - c.r), 
+						 max(0, g - c.g), 
+						 max(0, b - c.b));
 	}
 	
 	/**
@@ -55,7 +61,9 @@ public class Color {
 	 * @return
 	 */
 	public Color mul(final Color c) {
-		return null;
+		return new Color(r * c.r, 
+						 g * c.g, 
+						 b * c.b);
 	}
 	
 	/**
@@ -63,6 +71,47 @@ public class Color {
 	 * @return
 	 */
 	public Color mul(final double v) {
-		return null;
+		return new Color(max(0, min(1, r * v)), 
+						 max(0, min(1, g * v)), 
+						 max(0, min(1, b * v)));
+	}
+	
+	public String toString() {
+		return getClass().getSimpleName() + "[r = " + r + ", g = " + g + ", b = " + b + "]";
+	}
+	
+	//---- Test
+	public static void main(String[] args) {
+//		Color c01 = new Color(1, 1, -1);
+//		Color c02 = new Color(1, 1, 2);
+//		Color c03 = new Color(1, 1, Double.NaN);
+//		Color c04 = new Color(1, 1, Double.POSITIVE_INFINITY);
+		Color c0 = new Color(0, 0, 0);
+		Color c1 = new Color(1, 1, 1);
+		Color c2 = new Color(0.9, 0.9, 0.9);
+		Color c3 = new Color(0.009, 0.009, 0.009);
+		
+		System.out.println(c2.add(c1));
+		System.out.println(c2.add(c3));
+		System.out.println();
+		
+		System.out.println(c2.sub(c1));
+		System.out.println(c2.sub(c3));
+		System.out.println();
+		
+		System.out.println(c0.mul(c0));
+		System.out.println(c0.mul(c1));
+		System.out.println(c1.mul(c1));
+		System.out.println(c2.mul(c1));
+		System.out.println(c2.mul(c3));
+		System.out.println();
+		
+		System.out.println(c2.mul(0));
+		System.out.println(c2.mul(1));
+		System.out.println(c2.mul(0.9));
+		System.out.println(c2.mul(1.1));
+		System.out.println(c2.mul(10000));
+		System.out.println(c2.mul(-10000));
+		System.out.println();
 	}
 }
