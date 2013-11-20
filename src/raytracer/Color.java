daa4a9d2f5a878bbffad09e86bf89374a2e236a7
sebastian-dasse <s50602@beuth-hochschule.de>
2013-11-20 17:56:44 +0100

    Color: override hashCode() and equals().

--- src/raytracer/Color.java
+++ src/raytracer/Color.java
@@ -76,6 +76,38 @@ public class Color {
 						 max(0, min(1, b * v)));
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(b);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(g);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(r);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Color other = (Color) obj;
+		if (Double.doubleToLongBits(b) != Double.doubleToLongBits(other.b))
+			return false;
+		if (Double.doubleToLongBits(g) != Double.doubleToLongBits(other.g))
+			return false;
+		if (Double.doubleToLongBits(r) != Double.doubleToLongBits(other.r))
+			return false;
+		return true;
+	}
+
 	public String toString() {
 		return getClass().getSimpleName() + "[r = " + r + ", g = " + g + ", b = " + b + "]";
 	}
