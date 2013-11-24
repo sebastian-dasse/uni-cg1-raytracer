9a34cb5264e8f9ddf7d17071176b961105251516
Max Novichkov <s500@beuth-hochschule.de>
2013-11-24 10:06:57 +0100

    Add first part of calculation in Sphere

--- src/raytracer/geometry/Sphere.java
+++ src/raytracer/geometry/Sphere.java
@@ -25,13 +25,33 @@ public class Sphere extends Geometry {
 	 */
 	public Sphere(final Point3 c, final double r, final Color color) {
 		super(color);
+		if (c == null || r == 0) {
+			throw new IllegalArgumentException("The parameters must not be null.");
+		}
 		this.c = c;
 		this.r = r;
 	}
 
 	@Override
 	public Hit hit(final Ray ray) {
-		// TODO Auto-generated method stub
+		double a = ray.d.dot(ray.d);
+		if ( 2*a == 0){
+			return null;
+		}
+		double b = ray.d.dot((ray.o.sub(c)).mul(2.0));
+		double c1 = ray.o.sub(c).dot(ray.o.sub(c)) - r*r;
+		double d = b*b - 4*a*c1;	
+		if (d < 0) {
+			return null;
+		}
+		if (d == 0) {
+			double t = -b/2*a;
+			return new Hit(t, ray, this);
+		}
+		if (d > 0) {
+			
+		}
+		
 		return null;
 	}
 }
