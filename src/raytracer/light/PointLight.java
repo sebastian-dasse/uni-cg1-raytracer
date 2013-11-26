37db49123300a8d85cf2e2cbf6c81ac7035b9255
Simon Lischka <simon@a-studios.org>
2013-11-26 09:15:07 +0100

    Add remaining light classes

--- src/raytracer/light/PointLight.java
+++ src/raytracer/light/PointLight.java
@@ -1,5 +1,27 @@
 package raytracer.light;
 
-public class PointLight {
+import raytracer.Color;
+import raytracer.math.Point3;
+import raytracer.math.Vector3;
+
+public class PointLight extends Light {
+	public final Point3 position;
+
+	public PointLight(Color color, Point3 position) {
+		super(color);
+		this.position = position;
+	}
+	
+	@Override
+	public boolean illuminates(Point3 point) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Vector3 directionFrom(Point3 point) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/raytracer/light/SpotLight.java
+++ src/raytracer/light/SpotLight.java
@@ -1,5 +1,32 @@
 package raytracer.light;
 
-public class SpotLight {
+import raytracer.Color;
+import raytracer.math.Point3;
+import raytracer.math.Vector3;
 
+public class SpotLight extends Light {
+	public final Point3 position;
+	public final Vector3 direction;
+	public final double halfAngle;
+	
+	public SpotLight(Color color, Point3 position, Vector3 direction, double halfAngle) {
+		super(color);
+		this.position = position;
+		this.direction = direction;
+		this.halfAngle = halfAngle;
+	}
+
+	@Override
+	public boolean illuminates(Point3 point) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Vector3 directionFrom(Point3 point) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
