87bd288e8ef7a9a086d0bc9429712dc21cb1f4b8
sebastian-dasse <s50602@beuth-hochschule.de>
2013-11-26 09:48:09 +0100

    Doc-comment Light.

--- src/raytracer/light/Light.java
+++ src/raytracer/light/Light.java
@@ -5,39 +5,45 @@ import raytracer.math.Point3;
 import raytracer.math.Vector3;
 
 /**
- * 
+ * This abstract base class represents a light in a three-dimensional scene. 
+ * <p>
+ * It has a color and a method, which determines whether or not a point is illuminated by this light. It has another 
+ * method, that returns the vector which points from a specific point to this light.
  * 
  * @author 
  *
  */
 public abstract class Light {
 	/**
-	 * 
+	 * The color of this <code>Light</code>.
 	 */
 	public final Color color;
 	
 	/**
+	 * Constructs a new <code>Light</code> with the specified color.
 	 * 
-	 * 
-	 * @param color
+	 * @param color	The color of the <code>Light</code>. Must not be <code>null</code>.
 	 */
 	public Light(final Color color) {
+		if (color == null) {
+			throw new IllegalArgumentException("The parameter 'color' must not be null.");
+		}
 		this.color = color;
 	}
 	
 	/**
+	 * Returns <code>true</code> if the specified point is illuminated by this light.
 	 * 
-	 * 
-	 * @param point
-	 * @return
+	 * @param point	The point to be checked.
+	 * @return		<code>true</code> if the specified point is illuminated, otherwise <code>false</code>.
 	 */
 	public abstract boolean illuminates(final Point3 point);
 	
 	/**
+	 * Returns the vector which points from the specified point to this light.
 	 * 
-	 * 
-	 * @param point
-	 * @return
+	 * @param point	The point to be checked.
+	 * @return		A <code>Vector3</code>. // TODO or null? 
 	 */
 	public abstract Vector3 directionFrom(Point3 point);
 }
