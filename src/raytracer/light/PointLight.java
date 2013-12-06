81af79f5c1e31f663efc3445ab913e266a6cb38f
Simon Lischka <simon@a-studios.org>
2013-12-06 18:11:26 +0100

    Build point light class

--- src/raytracer/light/PointLight.java
+++ src/raytracer/light/PointLight.java
@@ -5,20 +5,23 @@ import raytracer.math.Point3;
 import raytracer.math.Vector3;
 
 /**
- * 
- * 
- * @author 
- *
+ * This class represents a point light in a three-dimensional scene.
+ * <p>
+ * It has a color and position. It includes a method to determine
+ * whether a point is illuminated by this light. 
+ * Another method returns the vector between the light position and the 
+ * point to be illuminated.
+ * <p>
+ * @author Simon Lischka
  */
 public class PointLight extends Light {
 	/**
-	 * 
+	 * A point to be illuminated
 	 */
 	public final Point3 position;
 
 	/**
-	 * 
-	 * 
+	 * Constructs a new <code>PointLight</code> object with the specified parameters.
 	 * @param color
 	 * @param position
 	 */
@@ -27,16 +30,21 @@ public class PointLight extends Light {
 		this.position = position;
 	}
 	
+	/**
+	 * Checks if the given point is illuminated by the light
+	 * @param point A point to be processed by the light source
+	 */
 	@Override
 	public boolean illuminates(final Point3 point) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
+	/**
+	 * Returns the vector from the point to the light source
+	 * @param point The point to be processed by the light source
+	 */
 	@Override
 	public Vector3 directionFrom(final Point3 point) {
-		// TODO Auto-generated method stub
-		return null;
+		return position.sub(point);
 	}
-
 }
