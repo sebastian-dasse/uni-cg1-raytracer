f31cbe3a47615c1d06475d3e93b721214dd7697c
sebastian-dasse <s50602@beuth-hochschule.de>
2013-12-07 16:47:46 +0100

    Another commit.

--- src/raytracer/material/PhongMaterial.java
+++ src/raytracer/material/PhongMaterial.java
@@ -62,7 +62,7 @@ public class PhongMaterial extends Material{
 		// TODO in Raytracer.normalizeColorComponent(...) verschieben
 		final double max1 = Math.max(world.ambientLight.r, world.ambientLight.g);
 		final double max2 = Math.max(max1, world.ambientLight.b);
-		c = (c.mul(1 / (lights.length + max2)));
+		c = (c.mul(1 / (lights.length * 2 + max2)));
 //		if (c.r > 1 || c.g >= 1 || c.b >= 1) {
 //			System.err.println(c);
 //		}
