edd8364d07f90031deabdef9e59c000d6d2dbe95
Simon Lischka <simon@a-studios.org>
2013-12-07 13:52:31 +0100

    Latest Phonging+

--- src/raytracer/tests/graphical/DemoScene.java
+++ src/raytracer/tests/graphical/DemoScene.java
@@ -9,6 +9,7 @@ import raytracer.camera.Camera;
 import raytracer.geometry.Geometry;
 import raytracer.light.PointLight;
 import raytracer.material.LambertMaterial;
+import raytracer.material.PhongMaterial;
 import raytracer.material.SingleColorMaterial;
 import raytracer.math.Point3;
 import raytracer.ui.ShowImage;
@@ -18,7 +19,8 @@ public final class DemoScene {
 	
 	public static void main(String[] args) {
 //		ShowImage.from(scene1());
-		ShowImage.from(scene2());
+//		ShowImage.from(scene2());
+		ShowImage.from(scene3());
 	}
 	
 	public static Raytracer scene1() {
@@ -54,6 +56,33 @@ public final class DemoScene {
 				Factory.buildTriangle(new double[][] { 
 						{ 0, 0, -1 }, { 1, 0, -1 }, { 1, 1, -1 }, 
 						{ 0, 0, 1 }, { 0, 0, 1 }, { 0, 0, 1 } }, new LambertMaterial(new Color(1, 1, 0)))
+			}
+		);
+//		world.addLight(new PointLight(new Color(1, 1, 1), new Point3(10, 10, 10)));
+//		world.addLight(new PointLight(new Color(1, 1, 1), new Point3(10, 10, 10)));
+//		world.addLight(new PointLight(new Color(1, 1, 1), new Point3(10, 10, 10)));
+		world.addLight(new PointLight(new Color(1, 1, 1), new Point3(10, 10, 10)));
+//		world.addLight(new PointLight(new Color(1, 1, 1), new Point3(-10, 10, 10)));
+//		world.addLight(new PointLight(new Color(1, 1, 1), new Point3(-10, -10, 10)));
+//		world.addLight(new PointLight(new Color(1, 1, 1), new Point3(0, 10, 0)));
+//		world.addLight(new PointLight(new Color(1, 0, 1), new Point3(-1, 10, 10))); // this s**t ain't gonna work
+		return new Raytracer(world, camera, size);
+	}
+	
+	public static Raytracer scene3() {
+		final World world = Factory.buildWorld(new double[][] { { 0, 0, 0 }, {0.1, 0.9, 0.1} });
+		final Camera camera = Factory.buildPerspectiveCamera(new double[][] {
+				{ 4, 4, 4 }, { -1, -1, -1 }, { 0, 1, 0 }, { Math.PI / 4.0 } });
+		world.addElements(new Geometry[] {
+				Factory.buildPlane(new double[][] { 
+						{ 0, 0, 0 }, { 0, 1, 0} }, new PhongMaterial(new Color(1, 0, 0), new Color(1,1,1), 64)),
+				Factory.buildSphere(new double[][] { 
+						{ 1, 1, 1 }, { 0.5 } }, new PhongMaterial(new Color(0, 1, 0), new Color(1,1,1), 64)),
+				Factory.buildAxisAlignedBox(new double[][] { 
+						{ -1.5, 0.5, 0.5 }, { -0.5, 1.5, 1.5 } }, new PhongMaterial(new Color(0, 0, 1), new Color(1,1,1), 64)), 
+				Factory.buildTriangle(new double[][] { 
+						{ 0, 0, -1 }, { 1, 0, -1 }, { 1, 1, -1 }, 
+						{ 0, 0, 1 }, { 0, 0, 1 }, { 0, 0, 1 } }, new PhongMaterial(new Color(1, 1, 0), new Color(1,1,1), 64))
 			}
 		);
 //		world.addLight(new PointLight(new Color(1, 1, 1), new Point3(10, 10, 10)));
