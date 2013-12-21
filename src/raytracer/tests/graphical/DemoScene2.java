6cf86040e070773aba515f3abe010685f57dea3f
sebastian-dasse <s50602@beuth-hochschule.de>
2013-12-21 19:16:21 +0100

    Add Constants class and DemoScene2.

---
+++ src/raytracer/tests/graphical/DemoScene2.java
@@ -0,0 +1,39 @@
+package raytracer.tests.graphical;
+
+import raytracer.Color;
+import raytracer.Raytracer;
+import raytracer.World;
+import raytracer.camera.Camera;
+import raytracer.geometry.Geometry;
+import raytracer.light.PointLight;
+import raytracer.material.PhongMaterial;
+import raytracer.math.Point3;
+import raytracer.ui.ShowImage;
+
+public class DemoScene2 {
+	
+	public static void main(String[] args) {
+		final Raytracer[] tracers = new Raytracer[]{
+				scene1()
+			};
+			for (int i = 0; i < tracers.length; i++) {
+				ShowImage.from(tracers[i], 50 * i, 25 * i);
+			}
+	}
+
+	public static Raytracer scene1() {
+		final World world = Factory.buildWorld(new double[][] { { 0, 0, 0 }, {0.1, 0.1, 0.1} }, 1);
+		final Camera camera = Factory.buildPerspectiveCamera(new double[][] {
+				{ 1, 1, 4 }, { -1, -1, -4 }, { 0, 1, 0 }, { Math.PI / 4.0 } });
+		world.addElements(new Geometry[] {
+				Factory.buildPlane(new double[][] { 
+						{ 0, 0, 0 }, { 0, 1, 0} }, new PhongMaterial(new Color(1, 0, 0), new Color(1, 1, 1), 64)),
+				Factory.buildSphere(new double[][] { 
+						{ 0, 0.5, 0 }, { 0.5 } }, new PhongMaterial(new Color(0, 1, 0), new Color(1, 1, 1), 64)),
+			}
+		);
+		world.addLight(new PointLight(new Color(0.6, 0.6, 0.6), new Point3(4, 4, 4)));
+		world.addLight(new PointLight(new Color(0.4, 0.4, 0.4), new Point3(-4, 4, 4)));
+		return new Raytracer(world, camera);
+	}
+}
