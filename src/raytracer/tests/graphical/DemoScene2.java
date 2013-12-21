a9e2f0242fb809174e5736cd1149d9eb5fe65bb5
sebastian-dasse <s50602@beuth-hochschule.de>
2013-12-21 19:58:37 +0100

    The latest in demo scenes.

--- src/raytracer/tests/graphical/DemoScene2.java
+++ src/raytracer/tests/graphical/DemoScene2.java
@@ -7,6 +7,7 @@ import raytracer.camera.Camera;
 import raytracer.geometry.Geometry;
 import raytracer.light.PointLight;
 import raytracer.material.PhongMaterial;
+import raytracer.material.SingleColorMaterial;
 import raytracer.math.Point3;
 import raytracer.ui.ShowImage;
 
@@ -20,8 +21,8 @@ public class DemoScene2 {
 				ShowImage.from(tracers[i], 50 * i, 25 * i);
 			}
 	}
-
-	public static Raytracer scene1() {
+	
+	private static Raytracer scene1() {
 		final World world = Factory.buildWorld(new double[][] { { 0, 0, 0 }, {0.1, 0.1, 0.1} }, 1);
 		final Camera camera = Factory.buildPerspectiveCamera(new double[][] {
 				{ 1, 1, 4 }, { -1, -1, -4 }, { 0, 1, 0 }, { Math.PI / 4.0 } });
@@ -29,11 +30,13 @@ public class DemoScene2 {
 				Factory.buildPlane(new double[][] { 
 						{ 0, 0, 0 }, { 0, 1, 0} }, new PhongMaterial(new Color(1, 0, 0), new Color(1, 1, 1), 64)),
 				Factory.buildSphere(new double[][] { 
-						{ 0, 0.5, 0 }, { 0.5 } }, new PhongMaterial(new Color(0, 1, 0), new Color(1, 1, 1), 64)),
+						{ 0, 1, 0 }, { 0.5 } }, new PhongMaterial(new Color(0, 1, 0), new Color(1, 1, 1), 64))
+//				Factory.buildAxisAlignedBox(new double[][] { 
+//						{ 0.5, 0.5, 0.5 }, { 1.5, 1.5, 1.5 } }, new PhongMaterial(new Color(0.3, 1, 0.3), new Color(1, 1, 1), 64)),
 			}
 		);
-		world.addLight(new PointLight(new Color(0.6, 0.6, 0.6), new Point3(4, 4, 4)));
-		world.addLight(new PointLight(new Color(0.4, 0.4, 0.4), new Point3(-4, 4, 4)));
+//		world.addLight(new PointLight(new Color(0.6, 0.6, 0.6), new Point3(4, 4, 4)));
+		world.addLight(new PointLight(new Color(0.3, 0.3, 0.3), new Point3(-4, 4, 4)));
 		return new Raytracer(world, camera);
 	}
 }
