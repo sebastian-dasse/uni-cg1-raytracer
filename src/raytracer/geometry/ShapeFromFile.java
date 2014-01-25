18301aec58308aab1a49d80f08b3351bfea40694
Simon Lischka <simon@a-studios.org>
2014-01-25 12:15:05 +0100

    Add node

---
+++ src/raytracer/geometry/ShapeFromFile.java
@@ -0,0 +1,16 @@
+package raytracer.geometry;
+
+import java.util.LinkedList;
+
+import raytracer.material.Material;
+import raytracer.math.Transform;
+
+public class ShapeFromFile extends Node {
+
+	public ShapeFromFile(Transform transform, LinkedList<Geometry> geos,
+			Material material) {
+		super(transform, geos, material);
+		// TODO Auto-generated constructor stub
+	}
+
+}
