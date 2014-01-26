4e1f46fcac1f6fbb2f545ae334030b13c392cf64
Simon Lischka <simon@a-studios.org>
2014-01-26 14:57:08 +0100

    more work

--- src/raytracer/tests/graphical/MySense.java
+++ src/raytracer/tests/graphical/MySense.java
@@ -16,10 +16,22 @@ public class MySense {
 	public final String TEXTURE = "vt";
 	public final String NORMAL = "vn";
 	public final String FACE = "f";
+	public final char COMMENT = '#';
+	LinkedList <String> lines;
+	private Collection<Point3> vertices;
+	private Collection<TextCoord> textures;
+	private Collection<Normal3> normals;
+	private Collection<String[]> faces;
 	
-	public Collection<String> readFile(File f) {
-		Collection<String> lines = new LinkedList<String>();
-		
+	public MySense() {
+		vertices = new LinkedList<Point3>();
+		textures = new LinkedList<TextCoord>();
+		normals = new LinkedList<Normal3>();
+		faces = new LinkedList<String[]>();
+		lines = new LinkedList<String>();
+	}
+	
+	public void readFile(File f) {
 		try {
 			Scanner in = new Scanner(f);
 			while (in.hasNext()) {
@@ -29,29 +41,49 @@ public class MySense {
 		catch (IOException e) {
 			System.err.println("Error reading file. Please check that you're not drunk.");
 		}
-		return lines;
 	}
 	
 	
-	public Collection<Point3> getVertices(Collection<String> lines) throws DataFormatException {
-		Collection<Point3> vertices = new LinkedList<Point3>();
-		Collection<TextCoord> textures = new LinkedList<TextCoord>();
-		Collection<Normal3> normals = new LinkedList<Normal3>();
-		Collection<String[]> faces = new LinkedList<String[]>();
-		for (String line : lines) {
+	public void parseBasicData() throws DataFormatException {
+		boolean facesEnd = false;
+		String previousType = "";
+		for (int lno = 0; lno < lines.size(); lno++) {
+			/*
+			 * Line processing
+			 */
+			String line = lines.get(lno);
 			String [] slots = line.split(" ");
+			
+			/*
+			 * Indicators
+			 */
 			final String type = slots[0];
-			if (type == FACE) {
-				if ((slots.length - 1) < 3) {
-					throw new DataFormatException();
-				}
-				faces.add(new String[] {
-						slots[1],
-						slots[2],
-						slots[3],
-				});
+			if (lno > 0) {
+				previousType = lines.get(lno - 1).split(" ")[0];
 			}
-			if (type != FACE) {
+			facesEnd = (previousType == FACE && type != FACE);
+			
+			/*
+			 * Special cases
+			 */
+			if (line.toCharArray()[0] == COMMENT) {
+				continue;
+			}
+			
+			if (type == FACE && (slots.length - 1) < 3) {
+				throw new DataFormatException();
+			}
+			
+			if (facesEnd) {
+				
+			}
+			
+			
+			if (type == FACE) {
+				
+				//
+				
+			} else {
 				double slotsAsDouble [] = new double [slots.length - 1];
 				for (int i = 1; i < slots.length; i++)  {
 					slotsAsDouble[i] = slots[i].isEmpty() ? Double.NaN : Double.parseDouble(slots[i]);
@@ -86,16 +118,17 @@ public class MySense {
 					);
 				}
 			}
-
 		}
-
-		return vertices;
+	}
+	
+	public void buildFaces() {
+		
 	}
 	
 	public static void main (String[] args) {
 //		
 //		int [] values = new int[3];
-		System.out.println(Double.parseDouble(""));
+		System.out.println(Double.parseDouble(Double.N));
 		
 	}
 }
