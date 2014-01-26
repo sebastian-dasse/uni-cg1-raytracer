cdf31d2c5948a5f4549d4a884cd19a1a3a43c625
Simon Lischka <simon@a-studios.org>
2014-01-26 13:48:31 +0100

    Some work on parser

--- src/raytracer/tests/graphical/MySense.java
+++ src/raytracer/tests/graphical/MySense.java
@@ -3,14 +3,19 @@ package raytracer.tests.graphical;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.IllegalFormatException;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.zip.DataFormatException;
 
+import raytracer.math.Normal3;
 import raytracer.math.Point3;
+import raytracer.texture.TextCoord;
 
 public class MySense {
 	public final String VERTICE = "v";
+	public final String TEXTURE = "vt";
+	public final String NORMAL = "vn";
+	public final String FACE = "f";
 	
 	public Collection<String> readFile(File f) {
 		Collection<String> lines = new LinkedList<String>();
@@ -28,37 +33,69 @@ public class MySense {
 	}
 	
 	
-	public Collection<Point3> getVertices(Collection<String> lines) {
+	public Collection<Point3> getVertices(Collection<String> lines) throws DataFormatException {
 		Collection<Point3> vertices = new LinkedList<Point3>();
+		Collection<TextCoord> textures = new LinkedList<TextCoord>();
+		Collection<Normal3> normals = new LinkedList<Normal3>();
+		Collection<String[]> faces = new LinkedList<String[]>();
 		for (String line : lines) {
 			String [] slots = line.split(" ");
-			if (slots[0] == VERTICE) {
-				vertices.add(
-						new Point3(
-								Double.parseDouble(slots[1]),
-								Double.parseDouble(slots[2]), 
-								Double.parseDouble(slots[3])
-						)
-				);
+			final String type = slots[0];
+			if (type == FACE) {
+				if ((slots.length - 1) < 3) {
+					throw new DataFormatException();
+				}
+				faces.add(new String[] {
+						slots[1],
+						slots[2],
+						slots[3],
+				});
 			}
+			if (type != FACE) {
+				double slotsAsDouble [] = new double [slots.length - 1];
+				for (int i = 1; i < slots.length; i++)  {
+					slotsAsDouble[i] = slots[i].isEmpty() ? Double.NaN : Double.parseDouble(slots[i]);
+				}
+
+				if (type == VERTICE) {
+					vertices.add(
+							new Point3(
+									Double.parseDouble(slots[1]),
+									Double.parseDouble(slots[2]), 
+									Double.parseDouble(slots[3])
+									)
+					);
+				}
+
+				if (type == TEXTURE) {
+					textures.add(
+							new TextCoord(
+									Double.parseDouble(slots[1]),
+									Double.parseDouble(slots[2])
+									)
+					);
+				}
+
+				if (type == NORMAL) {
+					normals.add(
+							new Normal3(
+									Double.parseDouble(slots[1]),
+									Double.parseDouble(slots[2]), 
+									Double.parseDouble(slots[3])
+									)
+					);
+				}
+			}
+
 		}
+
 		return vertices;
 	}
 	
 	public static void main (String[] args) {
 //		
 //		int [] values = new int[3];
-		System.out.println(values[2]);
+		System.out.println(Double.parseDouble(""));
 		
-		int [] values = {
-				1,
-				2,
-				3
-		};
-		
-		int [][] values2 = {
-				{1}{2}
-				}; 
-				
 	}
 }
