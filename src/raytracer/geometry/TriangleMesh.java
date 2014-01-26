8601e4fae6d892d5e77e47e41901705b3383b83b
sebastian-dasse <s50602@beuth-hochschule.de>
2014-01-26 13:45:56 +0100

    Further implementation of TriangleMesh.

--- src/raytracer/geometry/TriangleMesh.java
+++ src/raytracer/geometry/TriangleMesh.java
@@ -36,25 +36,36 @@ public class TriangleMesh extends Geometry {
 	public Hit hit(final Ray ray) {
 		
 		for (int[] face : faces) {
-			final int a = face[0];
-			final int b = face[3];
-			final int c = face[6];
+			final Point3 a = vertices[ face[0] ];
+			final Point3 b = vertices[ face[3] ];
+			final Point3 c = vertices[ face[6] ];
 			
-			final int at = face[1];
-			final int bt = face[4];
-			final int ct = face[7];
-			
-			final int an = face[2];
-			final int bn;
-			final int cn;
-			if (an != 0) {
-				bn = 0;
-				cn = 0;
+			/* //-- texture -- not yet used 
+			*/
+			final TextCoord at;
+			final TextCoord bt;
+			final TextCoord ct;
+			if (face[1] != 0) {
+				at = textCoords[ face[1] ];
+				bt = textCoords[ face[4] ];
+				ct = textCoords[ face[7] ];
 			} else {
-				bn = face[5];
-				cn = face[8];
+				at = bt = ct = new TextCoord(0, 0);
 			}
 			
+			final Normal3 an;
+			final Normal3 bn;
+			final Normal3 cn;
+			if (face[2] != 0) {
+				an = normals[ face[2] ];
+				bn = normals[ face[5] ];
+				cn = normals[ face[8] ];
+			} else {
+				an = bn = cn = b.sub(a).x(c.sub(a)).asNormal();
+			}
+			
+			Triangle tri = new Triangle(a, b, c, an, bn, cn, material);
+			Hit hit = tri.hit(ray);
 		}
 		
 		return null;
