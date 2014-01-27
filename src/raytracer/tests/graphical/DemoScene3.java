ba3fb68bece2f09d8b190e83b6c757b7dae70c49
Max Novichkov <s500@beuth-hochschule.de>
2014-01-27 18:21:07 +0100

    Merge

--- src/raytracer/tests/graphical/DemoScene3.java
+++ src/raytracer/tests/graphical/DemoScene3.java
@@ -37,37 +37,16 @@ public static void main(String[] args) {
 		
 		final Camera camera = Factory.buildPerspectiveCamera(new double[][] {
 				{ 4, 4, 4 }, { -1, -1, -1 }, { 0, 1, 0 }, { Math.PI / 4.0 } });
-//		Node first = new Node(
-//				new Sphere(new LambertMaterial(new Color(0, 1, 0))), 
-//				new Transform().scale(2.0, 1.0, 2.0));
-		//world.addElement(new Sphere(new Point3(0, 1, 0), 0.5, new PhongMaterial(new Color(0, 1, 0), new Color(1, 1, 1), 64)));
-		//world.addElement(new Plane(new Point3( 0, 0, 0), new Normal3(0, 1, 0), new PhongMaterial(new Color(1, 0, 0), new Color(1, 1, 1), 64)));
-//		world.addElement(new Node(
-//				new Sphere(new Point3(1, 3, 1), 0.5, new LambertMaterial(new Color(0, 1, 0))), 
-//				new Transform().scale(2.0, 1.0, 2.0)));
 		world.addElements(
 		new Node(
 				new AxisAlignedBox(new LambertMaterial(new Color(1, 1, 0))), 
 				new Transform()
-//				.rotateX(Math.PI/1.2)
-//				.rotateY(Math.PI/0.2) 
-//				.rotateZ(Math.PI/1.2)
-				
 		//ready transformed Box
 				.rotateX(Math.PI/1.17)
 				.rotateY(Math.PI/0.2) 
-				.rotateZ(Math.PI/1.15) //vokrug svoej osi
-				
-				
-//				.rotateX(Math.PI/1.2)
-//				.rotateY(Math.PI/0.2) 
-//				.rotateZ(Math.PI/1.15) //vokrug svoej osi
-
-//				.rotateY(Math.PI/0.2)
-//				.rotateZ(Math.PI/0.9)
-//				.rotateX(Math.PI/1.25)
+				.rotateZ(Math.PI/1.15) 
 				.scale(1.0, 0.26, 3.7)
-		//ready Sphere
+		//ready transformed Sphere
 //		new Node(
 //				new Sphere(new PhongMaterial(new Color(1, 0, 0), new Color(1, 1, 1), 64)), 
 //				new Transform()
