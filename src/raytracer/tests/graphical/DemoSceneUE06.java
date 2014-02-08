586e2f129f02960adb1722144809e2e34d394615
sebastian-dasse <s50602@beuth-hochschule.de>
2014-02-08 13:48:55 +0100

    Trial and error.

--- src/raytracer/tests/graphical/DemoSceneUE06.java
+++ src/raytracer/tests/graphical/DemoSceneUE06.java
@@ -6,6 +6,7 @@ import raytracer.Renderer;
 import raytracer.World;
 import raytracer.camera.Camera;
 import raytracer.camera.PerspectiveCamera;
+import raytracer.geometry.AxisAlignedBox;
 import raytracer.geometry.Node;
 import raytracer.geometry.Plane;
 import raytracer.light.PointLight;
@@ -38,12 +39,14 @@ public class DemoSceneUE06 {
 		final World world = new World(new Color(0, 0, 0), new Color(0.2, 0.2, 0.2), Constants.INDEX_OF_REFRACTION_AIR_AT_20_DEG);
 		world.addElements(
 				new Node(
-					new Plane(
+//					new Plane(
+					new AxisAlignedBox(
 //							new SingleColorMaterial(new SingleColorTexture(new Color(1, 0, 0)))
 //							new LambertMaterial(new SingleColorTexture(new Color(1, 0, 0)))
 							
 //							new SingleColorMaterial(new ImageTexture("textures/earth1.jpg"))
-							new SingleColorMaterial(new ImageTexture("textures/UE06-Textur.png"))
+//							new SingleColorMaterial(new ImageTexture("textures/testBild.png"))
+							new SingleColorMaterial(new ImageTexture("textures/bunny2.jpg"))
 //							new LambertMaterial(new SingleColorTexture(new Color(1, 0, 0)))
 					), 
 					new Transform()
@@ -51,7 +54,7 @@ public class DemoSceneUE06 {
 		);
 		world.addLight(new PointLight(new Color(1, 1, 1), new Point3(3, 3, 3), false));
 		
-		final Camera camera = new PerspectiveCamera(new Point3(4, 4, 4), new Vector3(-1, -1, -1), new Vector3(0, 1, 0), Math.PI / 4.0);
+		final Camera camera = new PerspectiveCamera(new Point3(2, 2, 2), new Vector3(-1, -1, -1), new Vector3(0, 1, 0), Math.PI / 4.0);
 		return new Renderer(world, camera, 10);
 	}
 }
