56a6ace32127a23747de8ddddcffe3e5713e75b0
Max Novichkov <s500@beuth-hochschule.de>
2014-02-08 12:39:17 +0100

    Merge

--- src/raytracer/tests/graphical/DemoScene3.java
+++ src/raytracer/tests/graphical/DemoScene3.java
@@ -4,6 +4,7 @@ import raytracer.Color;
 import raytracer.Renderer;
 import raytracer.World;
 import raytracer.camera.Camera;
+import raytracer.camera.PerspectiveCamera;
 import raytracer.geometry.AxisAlignedBox;
 import raytracer.geometry.Geometry;
 import raytracer.geometry.Node;
@@ -20,6 +21,7 @@ import raytracer.math.Normal3;
 import raytracer.math.Point3;
 import raytracer.math.Transform;
 import raytracer.math.Vector3;
+import raytracer.texture.ImageTexture;
 import raytracer.texture.SingleColorTexture;
 import raytracer.texture.TexCoord2;
 import raytracer.ui.ShowImage;
@@ -37,10 +39,10 @@ public static void main(String[] args) {
 	}
 	
 	private static Renderer scene1() {
-		final World world = Factory.buildWorld(new double[][] { { 0, 0, 0 }, {0.1, 0.1, 0.1} }, 1);
+		final World world = new World(new Color( 0, 0, 0), new Color(0.1, 0.1, 0.1), 1);
 		
-		final Camera camera = Factory.buildPerspectiveCamera(new double[][] {
-				{ 4, 4, 4 }, { -1, -1, -1 }, { 0, 1, 0 }, { Math.PI / 4.0 } });
+		final PerspectiveCamera camera = new PerspectiveCamera(new Point3(4, 4, 4), new Vector3(-1, -1, -1), new Vector3(0, 1, 0), 
+										Math.PI / 4.0);
 		world.addElements(
 				
 //				new Plane(
@@ -76,35 +78,36 @@ public static void main(String[] args) {
 //						new Transform()
 //						.translate(new Point3(-3.5, -1.5, 2.5))),
 //						
-//				new Node(new Sphere(new ReflectiveMaterial(
-//						new SingleColorTexture(new Color(0,0,1)), 
-//						new SingleColorTexture(new Color(1,0,0)), 
-//						64, 
-//						new SingleColorTexture(new Color(1,0,0)))), 
-//						new Transform()
-//						.translate(new Point3(0.5, 1.5, 1.5))),
+				new Node(new Sphere(new ReflectiveMaterial(
+						new ImageTexture("/raytracer-gottextures"), 
+						new SingleColorTexture(new Color(1,0,0)), 
+						64, 
+						new SingleColorTexture(new Color(1,0,0)))), 
+						new Transform()
+						.translate(new Point3(0.5, 1.5, 1.5))))
 						
-				new Node(new Plane(
+//				new Node(new Plane(
 //						new LambertMaterial(new SingleColorTexture(new Color(1,0,0)))),
 //						(new SingleColorMaterial(new SingleColorTexture(new Color(1,0,0)))),
-						new ReflectiveMaterial(
-								new SingleColorTexture(new Color(0,0,1)), 
-								new SingleColorTexture(new Color(0,1,0)), 
-								10, 
-								new SingleColorTexture(new Color(1,0,0)))), 
-								new Transform()
-								.translate(new Point3(0, 0, 0)
-								)),
+//						new ReflectiveMaterial(
+//								new SingleColorTexture(new Color(0,0,1)), 
+//								new SingleColorTexture(new Color(0,1,0)), 
+//								10, 
+//								new SingleColorTexture(new Color(1,0,0)))), 
+//								new Transform()
+//								.translate(new Point3(0, 0, 0)
+//								)),
 								
-				new Node(new Triangle( new Point3(0.7, 0.5, 3), new Point3(1.3, 0.5, 3), new Point3(0.7, 0.5, 4),
-						new Normal3(0, 1, 0), new Normal3(0, 1, 0), new Normal3(0, 1, 0), 
-						new SingleColorMaterial(new SingleColorTexture(new Color(1,0,1))), 
-						new TexCoord2(1,1), 
-						new TexCoord2(1,1), 
-						new TexCoord2(1,1)), 
-				new Transform()
-				.translate(new Point3(1, 2, -1)
-				)));
+//				new Node(new Triangle( new Point3(0.7, 0.5, 3), new Point3(1.3, 0.5, 3), new Point3(0.7, 0.5, 4),
+//						new Normal3(0, 1, 0), new Normal3(0, 1, 0), new Normal3(0, 1, 0), 
+//						new SingleColorMaterial(new SingleColorTexture(new Color(1,0,1))), 
+//						new TexCoord2(1,1), 
+//						new TexCoord2(1,1), 
+//						new TexCoord2(1,1)), 
+//				new Transform()
+//				.translate(new Point3(1, 2, -1)
+//				)))
+				;
 						
 						
 				
