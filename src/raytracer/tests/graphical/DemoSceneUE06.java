9e7709bd77c48d66775b393af270cded003eb54e
sebastian-dasse <s50602@beuth-hochschule.de>
2014-02-08 18:42:28 +0100

    Code simplification.

--- src/raytracer/tests/graphical/DemoSceneUE06.java
+++ src/raytracer/tests/graphical/DemoSceneUE06.java
@@ -6,15 +6,16 @@ import raytracer.Renderer;
 import raytracer.World;
 import raytracer.camera.Camera;
 import raytracer.camera.PerspectiveCamera;
-import raytracer.geometry.AxisAlignedBox;
 import raytracer.geometry.Node;
-import raytracer.geometry.Plane;
+import raytracer.geometry.Sphere;
 import raytracer.light.PointLight;
+import raytracer.material.LambertMaterial;
 import raytracer.material.SingleColorMaterial;
 import raytracer.math.Point3;
 import raytracer.math.Transform;
 import raytracer.math.Vector3;
 import raytracer.texture.ImageTexture;
+import raytracer.texture.Texture;
 import raytracer.ui.ShowImage;
 
 public class DemoSceneUE06 {
@@ -28,32 +29,45 @@ public class DemoSceneUE06 {
 		
 		
 		final Renderer[] tracers = new Renderer[]{
-				testScene()
+				scene1(), 
+//				scene2()
 		};
 		for (int i = 0; i < tracers.length; i++) {
 			ShowImage.from(tracers[i], 50 * i, 25 * i);
 		}
 	}
 
-	private static Renderer testScene() {
-		final World world = new World(new Color(0, 0, 0), new Color(0.0, 0.0, 0.0), Constants.INDEX_OF_REFRACTION_AIR_AT_20_DEG);
+	private static Renderer scene1() {
+		final String path = "textures/earth1.jpg";
+		final Texture texture = new ImageTexture(path);
+		final World world = new World(new Color(0, 0, 0), new Color(0.0, 0.0, 0.0), Constants.INDEX_OF_REFRACTION_VACUUM);
 		world.addElements(
 				new Node(
-//					new Plane(
-					new AxisAlignedBox(
-//							new SingleColorMaterial(new SingleColorTexture(new Color(1, 0, 0)))
-//							new LambertMaterial(new SingleColorTexture(new Color(1, 0, 0)))
-							
-//							new SingleColorMaterial(new ImageTexture("textures/earth1.jpg"))
-//							new SingleColorMaterial(new ImageTexture("textures/testBild.png"))
-							new SingleColorMaterial(new ImageTexture("textures/testBild2.jpg"))
-//							new LambertMaterial(new SingleColorTexture(new Color(1, 0, 0)))
-					), 
-					new Transform()
+						new Sphere(
+//								new SingleColorMaterial(texture)
+								new LambertMaterial(texture)
+						), 
+						new Transform()
 				)
 		);
 		world.addLights(new PointLight(new Color(1, 1, 1), new Point3(3, 3, 3), false));
-		
+		final Camera camera = new PerspectiveCamera(new Point3(2, 2, 2), new Vector3(-1, -1, -1), new Vector3(0, 1, 0), Math.PI / 4.0);
+		return new Renderer(world, camera, 10);
+	}
+	
+	private static Renderer scene2() {
+		final String path = "textures/earth1.jpg";
+		final Texture texture = new ImageTexture(path);
+		final World world = new World(new Color(0, 0, 0), new Color(0.0, 0.0, 0.0), Constants.INDEX_OF_REFRACTION_VACUUM);
+		world.addElements(
+				new Node(
+						new Sphere(
+								new SingleColorMaterial(texture)
+						), 
+						new Transform()
+				)
+		);
+		world.addLights(new PointLight(new Color(1, 1, 1), new Point3(3, 3, 3), false));
 		final Camera camera = new PerspectiveCamera(new Point3(2, 2, 2), new Vector3(-1, -1, -1), new Vector3(0, 1, 0), Math.PI / 4.0);
 		return new Renderer(world, camera, 10);
 	}
