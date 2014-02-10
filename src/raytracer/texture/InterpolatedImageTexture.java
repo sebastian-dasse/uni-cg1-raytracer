d8bb0a4cd63a3bd03920c7666a5d957e93b861f6
sebastian-dasse <s50602@beuth-hochschule.de>
2014-02-10 22:08:49 +0100

    Fix conflicts.

--- src/raytracer/texture/InterpolatedImageTexture.java
+++ src/raytracer/texture/InterpolatedImageTexture.java
@@ -2,29 +2,17 @@ package raytracer.texture;
 
 import raytracer.Color;
 /**
-<<<<<<< HEAD
- * This immutable class represents an image texture which is interpolated using bilinear interpolation. The image is 
- * loaded from a user specified file.
-=======
- * This immutable class represents a color of the image texture that
- * provides the texture data from loaded by user specified file.
- * This class supply ability to improve the texture quality by the interpolating of the
- * source data.
->>>>>>> 56f5eb6eb178cb2cdb725cca5ae2fa48b673ee02
+ * This immutable class represents an image texture which is interpolated using bilinear interpolation for improved 
+ * quality. The image is loaded from a user specified file.
  * 
  * @author Maxim Novichkov
  * @author Sebastian Dass&eacute;
  */
 public class InterpolatedImageTexture extends AbstractImageTexture {
 	/**
-<<<<<<< HEAD
 	 * Constructs a new <code>InterpolatedImageTexture</code>. Loads the specified image file when instantiated.
 	 * 
 	 * @param path	The path of the image. Must not be <code>null</code>.
-=======
-	 * Constructor of the texture. Loads file when instantiated. 
-	 * @param path The path used to load the file used in the texture.
->>>>>>> 56f5eb6eb178cb2cdb725cca5ae2fa48b673ee02
 	 */
 	public InterpolatedImageTexture(final String path){
 		super(path);
--- src/raytracer/texture/SingleColorTexture.java
+++ src/raytracer/texture/SingleColorTexture.java
@@ -2,12 +2,8 @@ package raytracer.texture;
 
 import raytracer.Color;
 /**
-<<<<<<< HEAD
  * This immutable class represents a texture showing a single color.
  * 
-=======
- * This immutable class represents a color of the simple color texture. 
->>>>>>> 56f5eb6eb178cb2cdb725cca5ae2fa48b673ee02
  * @author Maxim Novichkov
  *
  */
@@ -17,14 +13,9 @@ public class SingleColorTexture implements Texture{
 	 */
 	private final Color color;
 	/**
-<<<<<<< HEAD
 	 * Constructs a new <code>SingleColorTexture</code> with the specified color.
 	 * 
 	 * @param color	The color of the texture.
-=======
-	 * Construct a texture with only one color.
-	 * @param color The specified color of this texture.
->>>>>>> 56f5eb6eb178cb2cdb725cca5ae2fa48b673ee02
 	 */
 	public SingleColorTexture(final Color color){
 		if (color == null) {
